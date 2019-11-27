df15ce6fc6d9b03808be382da7fc85726b0651f4
ShaunKolich <employmeinc@gmail.com>
2019-11-27 16:05:44 -0500

    updating

---
+++ src/app/hero.service.ts
@@ -0,0 +1,19 @@
+import { HEROS } from './mock-heros';
+import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
+import { Hero } from './hero';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  constructor(private messageService: MessageService) {}
+
+  getHeroes(): Observable<Hero[]> {
+       this.messageService.add('HeroService: fetched heroes');
+    return of(HEROS);
+  }
+}
